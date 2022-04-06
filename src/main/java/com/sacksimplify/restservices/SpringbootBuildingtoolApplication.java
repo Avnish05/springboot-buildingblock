97367b76d88970f8deb70a0671dfd8ff482eb9c6
Avnish05 <avnishdubey0511@gmail.com>
2022-04-06 19:43:23 +0530

    First commit

---
+++ src/main/java/com/sacksimplify/restservices/SpringbootBuildingtoolApplication.java
@@ -0,0 +1,13 @@
+package com.sacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sacksimplify/restservices/SpringbootBuildingtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sacksimplify.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
