4a6d37a3381f37d304776942feb0300f8e358ed5
Avnish05 <avnishdubey0511@gmail.com>
2022-04-18 00:43:04 +0530

    JsonIgnore

--- src/main/java/com/sacksimplify/restservices/entities/User.java
+++ src/main/java/com/sacksimplify/restservices/entities/User.java
@@ -13,8 +13,12 @@ import javax.validation.constraints.Size;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "user")
+@JsonIgnoreProperties({"firstname","lastname"})
 public class User extends RepresentationModel<User>  {
 		
 		@Id
@@ -38,6 +42,7 @@ public class User extends RepresentationModel<User>  {
 		@Column(name = "ROLE", length=50, nullable=false)
 		private String role;
 		
+		@JsonIgnore
 		@Column(name = "SNN", length=50, nullable=false, unique=true)
 		private String ssn;
 
